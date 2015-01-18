69949958e2c51a1f1f2a11ebf5de52146bffd9db
Santo Tallarico <ShirleyPC@Shirley.telus>
2015-01-18 15:18:10 -0800

    Created trivial views & controllers, fixed image links and menu links Signed-off-by:Santo Tallarico <ShirleyPC@Shirley.telus>

---
+++ application/controllers/Gallery.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Gallery
+ *
+ * @author ShirleyPC
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gallery extends CI_Controller  {
+    //put your code here
+    public function index()
+	{
+		$this->load->view('gallery');
+	}
+}
